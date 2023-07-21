9cdef8c0cd001d97a4dc6f7323ba429423e94c6e
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-21 20:42:11 -0300

    variaveis

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -1,2 +1,34 @@
+console.log("Hello World");
+
+// console
+
 console.log("Console.log");
 console.error("console.error");
+console.warn("console.warn");
+
+
+// variaveris 
+
+var A = 10;
+var A = 20;
+
+let B = "Bruno"; // variavel, valor que pode mudar
+// let B = "aaaaa";
+B = "teste"; // reatribuição de valor
+
+const C = "constante"; //constante, não muda
+// const C = 22;
+// C = 30;
+
+if(true){
+    var A = 5;
+}
+
+console.log(A); // o var foi reescrito mesmo estando em escopos dierentes
+
+if(true){
+    let B = 10;
+    console.log(B); // esse let está em um escopo diferente
+}
+
+console.log(B); // vai imprimir o valor da variavel do escopo global
