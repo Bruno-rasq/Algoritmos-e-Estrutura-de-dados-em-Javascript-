319f084e8241e8cc0c016a48f047b2cc1dc2ab1b
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-21 22:29:03 -0300

    estruturas condicionais

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -165,3 +165,45 @@ console.warn("console.warn");
 // const obj2 = JSON.stringify(JSONerrado);
 // console.log(obj2);
 
+
+
+// // Estruuras condicionais
+// let A = 10;
+
+// if( A >= 10){
+//     console.log(" A maior que 8");
+// } else {
+//     console.log(" A não é maior que 8");
+// }
+
+// > maior que
+// < menor que
+// <= menor ou igual a 
+// >= maior ou igual a
+// == igual a (valor)
+// === igual a (tipo)
+// != diferente de
+// = receba!
+
+// const nome = "bruno";
+
+// if else
+// if(nome === "joao"){
+//     console.log("é joao");
+// } else {
+//     console.log(" não é joao");
+// };
+
+//if else if
+// if(nome == "bruno"){
+//     console.log("sim");
+// } else if(nome != "bruno"){
+//     console.log("sim, diferente");
+// };
+
+//if ternário
+// let number = 10;
+// let testeNumber = number >= 20 ? 'sim': 'não';
+// console.log(testeNumber);
+
+
