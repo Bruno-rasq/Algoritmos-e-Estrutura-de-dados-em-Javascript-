7d7af1315dfd5eb219ddc05e9727b29cdba26f3e
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-21 22:41:03 -0300

    estruturas de loop while e for

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -207,3 +207,25 @@ console.warn("console.warn");
 // console.log(testeNumber);
 
 
+//estruturas de loop/repetições
+// const mylist = [,2 ,3 ,5 ,74];
+// let contador = 0;
+
+// while(contador < mylist.length){
+
+//     console.log("imprimindo: " + mylist[contador]);
+
+//     // contador = contador + 1;
+//     contador ++
+// };
+
+
+// const listaDois = ["a", "b", "c", "d", "E"];
+
+// for (let index = 0; index < listaDois.length; index++) {
+//     // const element = array[index];
+
+//     // console.log(listaDois[index]);
+//     console.log(`imprimindo: ${listaDois[index]}`); // template literals
+    
+// };
