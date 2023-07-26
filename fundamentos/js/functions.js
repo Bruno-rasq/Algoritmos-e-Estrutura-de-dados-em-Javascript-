b26898e8096cfbf37bb1d797606ddce710607642
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-25 22:09:52 -0300

    iniciando o conceito de callbacks

--- fundamentos/js/functions.js
+++ fundamentos/js/functions.js
@@ -37,3 +37,27 @@ console.log(myArrowfunction(2, 7));
 
 const SimpleArrowFunction = (a, b) => a + b;
 console.log(SimpleArrowFunction(6, 7));
+
+
+
+// ============ CALLBACK FUNCTIONS ============
+
+// permite executar uma função depois de uma ação
+// sendo um conceito fundamental para entender a programação assincrona
+
+function exibir(num){
+    console.log("resultado da operação: " + num);
+}
+
+function soma(a, b, callback){
+    var op = a + b;
+    callback(op);
+};
+
+function multiplicacao(a, b, cb){
+    var op = a * b;
+    cb(op);
+};
+
+soma(2, 2, exibir);
+multiplicacao(5, 2, exibir);
--- fundamentos/script.js
+++ fundamentos/script.js
@@ -642,3 +642,28 @@ console.warn("console.warn");
 // console.log(pessoa.idade);
 
 // console.log(pessoa.saudacao());
+
+
+
+// ============ CALLBACK FUNCTIONS ============
+
+// permite executar uma função depois de uma ação
+// sendo um conceito fundamental para entender a programação assincrona
+
+function exibir(num){
+    console.log("resultado da operação: " + num);
+}
+
+function soma(a, b, callback){
+    var op = a + b;
+    callback(op);
+};
+
+function multiplicacao(a, b, cb){
+    var op = a * b;
+    cb(op);
+};
+
+soma(2, 2, exibir);
+multiplicacao(5, 2, exibir);
+
