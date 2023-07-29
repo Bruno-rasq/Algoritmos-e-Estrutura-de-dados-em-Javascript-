b6f7aefc9d68aa8da1deb84d117ca5329c709029
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-29 20:26:29 -0300

    cloneNode método usado para clonar um elemento html

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,1 +1,41 @@
-console.log("teste");
+// função que adiciona tarefa
+function addTask(){
+    
+    // titulo da tarefa
+    const TaskTitle = document.querySelector("#task-title").value;
+    
+    if(TaskTitle){
+
+        // clonagem do template
+        const Template = document.querySelector(".template");
+        const NewTask = Template.cloneNode(true);
+
+        // adiciona o titulo
+        NewTask.querySelector(".task-title").textContent = TaskTitle;
+
+        // removendo classes desnecessárias 
+        NewTask.classList.remove("template");
+        NewTask.classList.remove("hide");
+
+        // adicionar tarefa na lista
+        const list = document.querySelector("#task-list");
+
+        list.appendChild(NewTask);
+
+        //limpando o texto do input
+        document.querySelector("#task-title").value = "";
+    }
+
+}
+
+
+// evento adicionar tarefa
+const AddButton = document.querySelector("#add-btn");
+
+AddButton.addEventListener("click", (event) => {
+
+    event.preventDefault();
+
+    addTask();
+
+})
