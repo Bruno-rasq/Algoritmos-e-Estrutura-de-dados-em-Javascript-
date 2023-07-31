32a4b892f741abcba69df31556de46650d7da4a5
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-07-30 21:33:51 -0300

    mais alguns exercicios de javascript que consegui resolver

--- fundamentos/js/exercicios.js
+++ fundamentos/js/exercicios.js
@@ -155,3 +155,84 @@ function FindTheSmallest(arr){
 };
 
 FindTheSmallest([2, 6, 4, 8, 3, 21, 14]);
+
+
+// 02 - crie uma função que receba uma string como o nome de uma pessoa e devolva as iniciais
+// exm: "bruno dias" ---> "B.D";
+
+function GetInitials(name){
+
+    let split = name.split(' ');
+
+    let slice = split.map((name) => {
+        return name.slice(0, 1);
+    });
+
+    let response = slice.join('.').toUpperCase();
+    return console.log(response);
+
+};
+
+GetInitials("bruno dias");
+
+
+// 03 - função que receba uma string e devolva a memsa string inversa
+//porem somente a ordem das palavras tem que ser inversa, as palavras ainda devem ser legiveis
+
+function reverseAllWord(strg){
+
+    let split_words = strg.split(' ');
+    let reverse = split_words.reverse();
+
+    return console.log(reverse.join(" "));
+};
+
+reverseAllWord("dream big and dare to fail");
+
+// 04 - função que transcreva DNA para RNa
+// nota:na estrutura rna a terminação quimica (t) é substituida por (U);
+
+function translateDNA(dna){
+    let arr = dna.split('');
+    let response = arr.map((caracter) => {
+        return caracter.replace("T", "U");
+    });
+
+    return console.log(response.join(""));
+};
+
+translateDNA("CGATAT");
+translateDNA("GCAT");
+
+
+// 05 - função que receba um array com valores e retorne a media do array
+
+function getAverage(arr){
+    let response = 0;
+
+    for (n of arr) {
+        response += n;
+    };
+
+    let length = arr.length;
+
+    return console.log(response/length);
+};
+
+getAverage([1, 2, 3, 4, 5]);
+
+
+// 06 - função que recebe uma string e transcreve para a lingua do "P"
+
+function PLanguage(strg){
+    let split = strg.split(' ');
+
+    let response = split.map((s) => {
+        let p = "P";
+        return p + s;
+    });
+
+    return console.log(response.join(" "));
+};
+
+PLanguage("Simples é diferente de fácil");
