b265b26d859907ec20637fe4e36a7c17dc0bef7e
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-01 18:38:29 -0300

    algumas estruturas de regex

--- fundamentos/js/RegEx.js
+++ fundamentos/js/RegEx.js
@@ -118,3 +118,58 @@ let B = "G K T B P i P r p p R U ";
 console.log(B);
 
 RegExObject(B);
+
+
+
+
+
+let texto = "isto é um texto, e este texto será usado para testar o funcionamento de regEx em texto";
+let regEx = /texto/g;
+
+let response = texto.match(regEx);
+console.log(response);
+
+let response2 = regEx.exec(texto);
+console.log(response2);
+
+let response3 = regEx.test(texto);
+console.log(response3);
+
+
+// =========== expressions flag ============
+
+// expressions flag são estes carcateres que aparecem depois da barra de fechamento
+// ele habilitam alguma funcionalidade extra para a expressão
+
+// por exemplo o G que faz com que o todas ocorencias valida da expressão apareça
+// ou o I que ativa o case Insensitive, possibilitando pegar letras mausculas e minusculas
+
+
+// ==========================================
+
+// . -> ponto sozinho pega todos os caracteres, como o * global do css e etc...
+// uma letra e um ponto "A." pega todos os A e o caracter sucessor seja ele qualquer
+
+// | -> significa "ou" exm: o | ó pega letra o com ou sem acento
+// () -> agrupa o comando em um subgrupo
+// [] -> vai buscar poelas ocorrencias dentro de colchetes ex: [abc] vai procurar pelas letra abc juntas ou separadas
+
+
+// sequencias são feitas atravez so traço [a-z] carcateres de a ate z, ou [0-9] numeros de 0 a 9
+// ^ funciona como uma negação exm: [^ex] pega todos os carcateres menos o E e o X
+// (e+) o + nesse caso significa a letra e uma vez ou mais de uma vez, agrupando quando tiver mais de uma ocorrencia
+// ? serve como um opcional
+
+
+// {}  o valor que estiver dentro de chaves significa a quantidade de vezes que um determinado grupo deverá aparecer
+// exm:   e{2}   vai pegar apenas as ocorrencias que tenha dois E juntos
+// exm:   e{2,}  pega todas as ocorrencias que tenho 2 Es ou mais
+// exm:   e{2,5} pega todas as ocorrencias em que o E aparece no minimo duas vezes e no máximo 5
+
+
+// ^texto   verifica se o texto escrito começa com 'texto'
+// texto$   verifica se o texto escrito termina com 'texto
+
+//  \       serve para expressões literais
+// emx: o ponto(.) pega todas os caracteres, porem se quiser pegar literalmente um ponto(.) utiliza-se a contra barra
+// /\./
--- fundamentos/script.js
+++ fundamentos/script.js
@@ -6,49 +6,53 @@ console.error("console.error");
 console.warn("console.warn");
 
 
-// =========== regex =============
+let texto = "isto é um texto, e este texto será usado para testar o funcionamento de regEx em texto";
+let regEx = /texto/g;
+
+let response = texto.match(regEx);
+console.log(response);
+
+let response2 = regEx.exec(texto);
+console.log(response2);
+
+let response3 = regEx.test(texto);
+console.log(response3);
 
 
-// duas formas de criar expressões regular: expressão literal e atravez do constructor do objeto
+// =========== expressions flag ============
 
-//[expressão literal]
+// expressions flag são estes carcateres que aparecem depois da barra de fechamento
+// ele habilitam alguma funcionalidade extra para a expressão
 
-// expressões simples são aquelas que são iniciada e fechadas com barra /
-// exm: /[t,t]/ 
-//      /[0-9]{4}/
-
-function RegExLiteral(strg){
-
-    //substituindo todos os "P" por B, com expressão literal
-    let response = strg.replace(/[p,P]/g, "B");
-    return console.log(response);
-
-};
-
-let arr = "B t U F P B P Y r p";
-console.log(arr);
-
-RegExLiteral(arr);
-console.log( /[p,P]/);
-console.log( typeof /[p,P]/);
+// por exemplo o G que faz com que o todas ocorencias valida da expressão apareça
+// ou o I que ativa o case Insensitive, possibilitando pegar letras mausculas e minusculas
 
 
+// ==========================================
+
+// . -> ponto sozinho pega todos os caracteres, como o * global do css e etc...
+// uma letra e um ponto "A." pega todos os A e o caracter sucessor seja ele qualquer
+
+// | -> significa "ou" exm: o | ó pega letra o com ou sem acento
+// () -> agrupa o comando em um subgrupo
+// [] -> vai buscar poelas ocorrencias dentro de colchetes ex: [abc] vai procurar pelas letra abc juntas ou separadas
 
 
-//[chamando o constructor do objeto regex]
+// sequencias são feitas atravez so traço [a-z] carcateres de a ate z, ou [0-9] numeros de 0 a 9
+// ^ funciona como uma negação exm: [^ex] pega todos os carcateres menos o E e o X
+// (e+) o + nesse caso significa a letra e uma vez ou mais de uma vez, agrupando quando tiver mais de uma ocorrencia
+// ? serve como um opcional
 
-// vale citar que no javascript o regexé considerado um objeto tbm
 
-function RegExObject(B){
+// {}  o valor que estiver dentro de chaves significa a quantidade de vezes que um determinado grupo deverá aparecer
+// exm:   e{2}   vai pegar apenas as ocorrencias que tenha dois E juntos
+// exm:   e{2,}  pega todas as ocorrencias que tenho 2 Es ou mais
+// exm:   e{2,5} pega todas as ocorrencias em que o E aparece no minimo duas vezes e no máximo 5
 
-    const Regex = new RegExp("P");
 
-    let response = B.replace(Regex, "A");
-    return console.log(response);
-};
+// ^texto   verifica se o texto escrito começa com 'texto'
+// texto$   verifica se o texto escrito termina com 'texto
 
-let B = "G K T B P i P r p p R U ";
-console.log(B);
-
-RegExObject(B);
-
+//  \       serve para expressões literais
+// emx: o ponto(.) pega todas os caracteres, porem se quiser pegar literalmente um ponto(.) utiliza-se a contra barra
+// /\./
