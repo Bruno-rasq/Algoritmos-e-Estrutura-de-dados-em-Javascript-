5668acdf4377d732816508f479305f91235d2707
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-01 19:06:50 -0300

    um exemplo de regEx para email e telefone

--- fundamentos/js/RegEx.js
+++ fundamentos/js/RegEx.js
@@ -173,3 +173,10 @@ console.log(response3);
 //  \       serve para expressões literais
 // emx: o ponto(.) pega todas os caracteres, porem se quiser pegar literalmente um ponto(.) utiliza-se a contra barra
 // /\./
+
+
+let email = "bruno@gmail.com.br";
+let RegEx_email = /([a-z0-9\.\-]{2,})@([a-z]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?/gi;
+
+let telefone = "(45) 98888-8888";
+let regEx_telefone = /\([0-9]{2}\)\s([9]{1})?([0-9]{3,4})\-([0-9]{3,4})/g;
--- fundamentos/script.js
+++ fundamentos/script.js
@@ -6,53 +6,8 @@ console.error("console.error");
 console.warn("console.warn");
 
 
-let texto = "isto é um texto, e este texto será usado para testar o funcionamento de regEx em texto";
-let regEx = /texto/g;
+// let email = "bruno@gmail.com.br";
+// let RegEx_email = /([a-z0-9\.\-]{2,})@([a-z]{2,})(\.[a-z]{2,})(\.[a-z]{2,})?/gi;
 
-let response = texto.match(regEx);
-console.log(response);
-
-let response2 = regEx.exec(texto);
-console.log(response2);
-
-let response3 = regEx.test(texto);
-console.log(response3);
-
-
-// =========== expressions flag ============
-
-// expressions flag são estes carcateres que aparecem depois da barra de fechamento
-// ele habilitam alguma funcionalidade extra para a expressão
-
-// por exemplo o G que faz com que o todas ocorencias valida da expressão apareça
-// ou o I que ativa o case Insensitive, possibilitando pegar letras mausculas e minusculas
-
-
-// ==========================================
-
-// . -> ponto sozinho pega todos os caracteres, como o * global do css e etc...
-// uma letra e um ponto "A." pega todos os A e o caracter sucessor seja ele qualquer
-
-// | -> significa "ou" exm: o | ó pega letra o com ou sem acento
-// () -> agrupa o comando em um subgrupo
-// [] -> vai buscar poelas ocorrencias dentro de colchetes ex: [abc] vai procurar pelas letra abc juntas ou separadas
-
-
-// sequencias são feitas atravez so traço [a-z] carcateres de a ate z, ou [0-9] numeros de 0 a 9
-// ^ funciona como uma negação exm: [^ex] pega todos os carcateres menos o E e o X
-// (e+) o + nesse caso significa a letra e uma vez ou mais de uma vez, agrupando quando tiver mais de uma ocorrencia
-// ? serve como um opcional
-
-
-// {}  o valor que estiver dentro de chaves significa a quantidade de vezes que um determinado grupo deverá aparecer
-// exm:   e{2}   vai pegar apenas as ocorrencias que tenha dois E juntos
-// exm:   e{2,}  pega todas as ocorrencias que tenho 2 Es ou mais
-// exm:   e{2,5} pega todas as ocorrencias em que o E aparece no minimo duas vezes e no máximo 5
-
-
-// ^texto   verifica se o texto escrito começa com 'texto'
-// texto$   verifica se o texto escrito termina com 'texto
-
-//  \       serve para expressões literais
-// emx: o ponto(.) pega todas os caracteres, porem se quiser pegar literalmente um ponto(.) utiliza-se a contra barra
-// /\./
+// let telefone = "(45) 98888-8888";
+// let regEx_telefone = /\([0-9]{2}\)\s([9]{1})?([0-9]{3,4})\-([0-9]{3,4})/g;
