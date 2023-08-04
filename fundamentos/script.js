abbb3b380d99db747323d62379b3b9c145876702
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-04 20:19:02 -0300

    POO instanciando classe e herança

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -5,8 +5,55 @@ console.log("Console.log");
 console.error("console.error");
 console.warn("console.warn");
 
-function testObject([first, ...rest]){
-    console.log((rest.join('')) + first);
+class person {
+    constructor(name, age, sex){
+        this.name = name,
+        this.age = age,
+        this.sex = sex
+    }
+
+    showperson(){
+        return console.log(`Olá, meu nome é ${this.name} eu tenho ${this.age} anos de idade.`);
+    }
+
+    //estaticos
+    static helloWorld(){
+        console.log("hello World!");
+    }
 };
 
-testObject("banana");
+const johnDoe = new person("john Doe", 34, "male");
+console.log(johnDoe);
+johnDoe.showperson();
+
+// acessando métodos estáticos
+person.helloWorld();
+
+
+// ===============================  [herança] ============================
+
+class animal {
+    constructor(name){
+        this.name = name
+    }
+
+    MadeSomeNoise(){
+        console.log(`${this.name} made some noise`);
+    }
+}
+
+class rabbit extends animal{
+    constructor(name){
+        super(name)
+    }
+
+    MadeSomeNoise(){
+        console.log(`Rabbit ${this.name} Made some squeak`);
+    }
+}
+
+const osvald = new animal("Osvald");
+osvald.MadeSomeNoise();
+
+const Rabbit = new rabbit("Osvaldo");
+Rabbit.MadeSomeNoise();
