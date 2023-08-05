c0bbbc3a729a9644c3f8571aa1c9917073ebb6ba
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-04 23:03:50 -0300

    nao resolvido

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -6,43 +6,13 @@ console.error("console.error");
 console.warn("console.warn");
 
 
-function HideEmail(email){
+// não resolvido
+function AllEquals(arr){
 
-    let ahoba = email.indexOf("@");
-    let partOne = email.slice(0, ahoba);
-    let partTwo = email.slice(ahoba + 1, email.length);
-
-    let Part_one_split = partOne.split('');
-    let inicial = Part_one_split[0];
-    Part_one_split.shift();
-    let req_one = Part_one_split.map((char) => {
-        return char = "*"
-    }).join('');
-
-    let resp_one = inicial + req_one;
-
-    let Part_Two_split = partTwo.split('');
-    let last = Part_Two_split[Part_Two_split.length -1];
-    Part_Two_split.pop();
-    let req_two = Part_Two_split.map((char) => {
-        return char = "*"
-    }).join('');
-
-    let resp_two = req_two + last;
-
-    let response = resp_one + "@" + resp_two;
+    let response = arr.every((n) => n === n);
 
     return console.log(response);
 };
 
-HideEmail("johndoe@gmail.com");
-HideEmail("bruno@gmail.com");
-HideEmail("meumanoheleno@gmail.com.br");
-
-//.pop() remove ultimo elemento do array
-//.push() adiciona um elemento no final do array
-
-//.shift() remove o primeiro elemento do array
-//.unshift() adiciona um elemento no inicio do array;
-
-//.splice() remove, adiciona ou substitui elemento em posição especifica de array
+AllEquals([1, 1, 1, 1, 1, 1]);
+AllEquals([1, 3, 1, 2, 1, 1]);
