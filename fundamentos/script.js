8c97baf608d387defffbaed79e9475bdf2e16dca
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-06 23:29:10 -0300

    conceito de clear code - duplicação de código e comentários

--- fundamentos/script.js
+++ fundamentos/script.js
@@ -71,3 +71,76 @@ if(true){
 
 // evitar redundancia
 // evitar os desnecessário
+
+//jeito errado 
+
+//função que sma dois numeros
+function SomarNumeros(a, b){
+    return a + b;
+};
+
+const results = x < 0 ? 0 : x > 100 ? 100 : x;
+
+
+// jeito certo 
+
+/*
+    * somar dois numero e retornar resultado
+    * @param {number} a - o primeiro numero
+    * @param {number} b - o segundo numero
+    * @return {number} - o resultado da soma de a e b
+*/
+function SomarNumerosCerto(a, b){
+    return a + b;
+};
+
+// garante que o x esteja entre 0 e 100
+const resultsCerto = x < 0 ? 0 : x > 100 ? 100 : x;
+
+
+
+
+// [ 04 - duplicação de código ] ================
+
+// dificulta manutenção, atualização e leitura
+
+// solução refatoração
+
+const SPACE = " ";
+
+function getFullName( firstName, lastName){
+    let fullName = firstName + SPACE + lastName;
+    return fullName;
+}
+
+function getFullName2( firstName, lastName){
+    let fullName = firstName + SPACE + lastName;
+    return fullName;
+}
+
+
+
+function getPrice(product){
+    let price;
+
+    if( product === "caneta"){
+        price = 20;
+    } else if ( product === "apagador"){
+        price = 10;
+    } else if ( product === "lápis"){
+        price = 5;
+    }
+
+    return price;
+}
+
+
+const products = {
+    caneta: 20,
+    apagador: 10,
+    lapis: 5,
+}
+
+function GetProductsPrice(product){
+    return products[product];
+}
