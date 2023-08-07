9176f154dbbc91128da2bc985cbd7a96053e5994
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-06 21:22:17 -0300

    versao correta da funcao de esconder email

--- fundamentos/js/exercicios.js
+++ fundamentos/js/exercicios.js
@@ -368,6 +368,32 @@ HideEmail("meumanoheleno@gmail.com.br");
 
 //.splice() remove, adiciona ou substitui elemento em posição especifica de array
 
+function HideEmail2(email){
+
+    let arr = email.split("@");
+    let part1 = arr[0].split('');
+    let part2 = arr[arr.length -1].split('');
+
+    let inicial = part1[0];
+    let last = part2[part2.length -1];
+
+    part2.pop();
+    part1.shift();
+
+    let req1 = inicial + part1.map((char) => {
+        return char = "*";
+    }).join('');
+
+    let req2 = part2.map((char) => {
+        return char = "*";
+    }).join('') + last;
+
+    return console.log( req1 + "@" + req2);
+}
+
+HideEmail2("marie@gmail.com");
+HideEmail2("johndoe@gmail.com");
+
 
 // 02 - função que troque as vogais por numeros de 1 a 5
 // a=1, e=2, i=3, o=4, u=5
--- fundamentos/script.js
+++ fundamentos/script.js
@@ -6,16 +6,28 @@ console.error("console.error");
 console.warn("console.warn");
 
 
-function findSecond(list){
-    list.sort((a,b) => {
-        if(a > b) return 1;
-        if(a < b) return -1;
-        return 0;
-    });
+function HideEmail(email){
 
-    let response = list[list.length - 2];
-    return console.log(response);
+    let arr = email.split("@");
+    let part1 = arr[0].split('');
+    let part2 = arr[arr.length -1].split('');
+
+    let inicial = part1[0];
+    let last = part2[part2.length -1];
+
+    part2.pop();
+    part1.shift();
+
+    let req1 = inicial + part1.map((char) => {
+        return char = "*";
+    }).join('');
+
+    let req2 = part2.map((char) => {
+        return char = "*";
+    }).join('') + last;
+
+    return console.log( req1 + "@" + req2);
 }
 
-findSecond([42, 2, 57, 1000]);
-findSecond([3, 4, 67, 8, 9, 10]);
+HideEmail("marie@gmail.com");
+HideEmail("johndoe@gmail.com");
