1dc0b65fa09b6da0c6b39862d207f88e31c67dd4
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-07 22:11:18 -0300

    funcao que remove caracteres duplicados de uma string

--- codict-challenge/hackear.js
+++ codict-challenge/hackear.js
@@ -16,3 +16,57 @@ function anagramDetector(str1, str2){
 anagramDetector("car", "arc");
 anagramDetector("race", "car");
 
+
+/*
+    02 - Unique characters
+    @function hasUniqueCharacter(str)
+    a função deverá receber uma string e verificar se todos os caracteres são unicos
+    retornando um booleano
+
+    test 1 -> "abcdef" => true
+    test 2 -> "aAbBcC" => false
+
+*/
+
+
+/*
+    03 - compress string
+    @function compressString(str)
+    a função deverá receber uma string e se houver carcateres repetidos em sequencia
+    deverá comprimi-los ao caracter e um numero que representa a quantidade de vezes que 
+    se repete
+
+    test 1 -> "aabbcc" => "a2b2c2"
+    test 2 -> "aaabbbccc" => "a3b3c3"
+
+*/
+
+/*
+    04 - two sum
+    @function twoSum(nums, target)
+    a função receberá dois parametros @nums que é uma lista de valores e @target que é um alvo
+    então deverá verificar se em @nums possue valores menores que @target, então retornar o indice
+    deles no array
+
+    test 1 -> ([2, 4, 5], 6) => [0, 1, 2]
+    test 2 -> ([3, 3], 6) => [0, 1]
+
+*/
+
+/*
+    05 - romeve duplicates
+    @function removeDups(str)
+    a funçaõ receberá uma string e deverá retornar uma nova string removendo os caracteres duplicados
+
+    test 1 -> ("abcdef") => "abcdef"
+    test 2 -> ("abccdeefkkk") => "abcdefk"
+
+*/
+
+function removeDups(str){
+    let response = str.replace(/(.)(?=.*\1)/g, "")
+    return console.log(response);
+}
+
+removeDups("abcdef");
+removeDups("abccdeefkkk");
