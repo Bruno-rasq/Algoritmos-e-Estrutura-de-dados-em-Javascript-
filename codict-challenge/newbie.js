685b47ee9e8f2e874405536ea7a920817c92b6e8
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-09 20:57:25 -0300

    nivel newbie concluido

--- codict-challenge/newbie.js
+++ codict-challenge/newbie.js
@@ -1,16 +1,33 @@
-// [nivel iniciante - NEWBIE]
+/*
+    01 - sum
+    @function sum(n1, n2)
+    a função receberá dois numero positivos como parametro @n1 e @n2 e retornar
+    a soma dos numeros
 
-// 01 - função para somar dois valores 
+    test 1 - (3, 5) => 8
+    test 2 - (2, 3) => 5
 
+*/
+//[RESPOSTA 01]
 function somar(n1, n2){
     return console.log(n1 + n2);
 };
 
 somar(3, 5);
+somar(2, 3);
 
-// 02 - criar uma função que pegue os valores de uma array numerico, filtrando valores positivos
-// e depois somando-os
 
+
+/*
+    02 - somar positivos de array
+    @function somaPositivosArray(array)
+    criar uma função que pegue os valores de uma array numerico, filtrando valores positivos
+    e depois somando-os
+
+    test 1 - ([5, -4, 10, -2, -5]) => 15
+    test 2 - ([2, 2, 2, -2, -1]) => 6
+*/
+//[RESPOSTA 02]
 function SomaPositivosArray(arry){
     let newarray = arry.filter((number) => {
         return number >= 0;
@@ -22,10 +39,22 @@ function SomaPositivosArray(arry){
     console.log("exercicio 1 " + total);
 };
 
-SomaPositivosArray([,5 ,-4 ,10, -2, -5]);
+SomaPositivosArray([5 ,-4 ,10, -2, -5]);
 
-// 03 - criar uma função que verifica se um valor é par ou impar
 
+
+
+/*
+    03 - par oun impar
+    @function ParOuImpar(n)
+    a função receberá um numero como parametro @n e deverá retornar
+    se o numero é par ou impar
+
+    test - 1 (8) => par
+    test - 2 (3) => impar
+
+*/
+//[RESPOSTA 03]
 function ParOuImpar(num){
     if(num % 2 == 0){
         // console.log("exercicio 2: o numero " + num + " é PAR ");
@@ -38,17 +67,39 @@ function ParOuImpar(num){
 
 ParOuImpar(8);
 
-// 04 - criar uma função que modifique a area de um triangulo
 
+
+
+/*
+    04 - triangle area
+    @function triangleArea(b, h)
+    a função receberá dois parâmetros base @b e altura @h, e com base
+    nesse valores calcular a area de um triângulo.
+
+    test - 1 (10, 5) => 25
+    test - 2 (5, 5) => 12,5
+
+*/
+//[RESPOSTA 04]
 function TrinagleArea(b, h){
     let area = (b*h)/2;
     return area;
 
 };
 
-// 05 - retornar um valor com sinal oposto
-// exm: 3 deverá retornar -3 // -4 deverá retornar 4
 
+
+/*
+    05 - sinal oposto
+    @function returnnegative(num)
+    a função receberá um valor numerico como parâmetro @num
+    e deverá retornar o valor com o sinal oposto
+
+    test - 1 (8) => -8
+    test - 2 (-8) => 8
+
+*/
+//[RESPOSTA 05]
 function ReturnNegative(num){
 
     return console.log(-num);
@@ -57,9 +108,20 @@ function ReturnNegative(num){
  
  ReturnNegative(50);
 
-  // 06 - função que pegue um valor em anos e retorne um valor em dias
 
-  function ageToDays(age){
+ 
+/*
+    06 - anos para dias
+    @function ageToDays(age) 
+    a função recebrá um valor positivo como parâmetro @age e deverá converter
+    esse valor de anos para dias.
+
+    test - 1 (4) => 1460
+    test - 2 (1) => 365
+
+*/
+//[RESPOSTA 06]
+function ageToDays(age){
     let days = (age * 365);
 
     return console.log(days);
@@ -68,8 +130,20 @@ function ReturnNegative(num){
 ageToDays(4);
 
 
-// 07 -  função que retorna qualquer valor com sinal negativo
 
+// 07 -  função que retorna qualquer valor com sinal negativo
+/*
+    07n - tone tudo negativo
+    @function TurnToNegative(num)
+    a função receberá um valor positivo ou negativo como parâmetro @num
+    e deverá returnar negativo
+
+    test - 1 (10) => -10
+    test - 2 (-6) => -6
+
+
+*/
+//[RESPOSTA 07]
 function TurnToNegative(num){
 
     if(num < 0){
@@ -82,7 +156,17 @@ function TurnToNegative(num){
 
 
 // 08 - função que pegue uma string e remova todos os espaços em branco
+/*
+    08 - remova os espaços
+    @function spaceRemove(strg)
+    a função recebe uma string como parâmetro @strg e deverá retornar a mesma string 
+    porém removido todos os espaços em branco
 
+    test -1 ("  testando se funciona") => "testandosefunciona"
+    test -2 ("teste dois") => "testedois"
+
+*/
+//[RESPOSTA 08]
 function spaceRemove(strg){
     //  regEx /\s+/ serve para selecionar os espaços
     let response = strg.split(/\s+/).join('');  // utiliza expressões regulares
@@ -93,8 +177,18 @@ function spaceRemove(strg){
 spaceRemove("   testendo se funciona");
 
 
-// 09 - função que encontre o nome "john" dentro de um array
 
+/*
+    09 - encontre john
+    @function findJohn(arry)
+    a função recebe um array com nomes de pessoas como parâmetro @arry
+    e deverá um boolean para caso o nome "john" esteja no array ou não
+
+    test -1 (["maria", "clara", "bruno", "john"]) => true
+    test -2 (["maria", "clara", "bruno"]) => false
+
+*/
+//[RESPOSTA 09]
 let array = ["maria", "clara", "bruno", "john"];
 let array2 = ["maria", "clara", "bruno"];
 
@@ -105,12 +199,20 @@ function FindJohn(arry){
 };
 
 FindJohn(array2);
+FindJohn(array);
 
 
-// 010 -  função que pegue os valores de vitorias, empates e derrotas  e returnasse a somatoria de pontos
-// win = 3 pontos
-// draw = 1 ponto
-// loss = 0 pontos
+
+/*
+    010 - placar do jogo
+    @function SumPoints(w, d, l)
+    a função recebe três variaveis como parâmetro @w, @d e @l, respectivamente os valores
+    de vitorias, empates e derrotas. então retorne a somatoria dos pontos
+        // win = 3 pontos
+        // draw = 1 ponto
+        // loss = 0 pontos
+
+*/
 
 function SumPoints(w, d, l){
     let win = w * 3;
