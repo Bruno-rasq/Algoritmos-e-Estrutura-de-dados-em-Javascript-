7262c7b67c07a7f2922cabef8b99d238ddbf848d
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-08-12 17:19:13 -0300

    conversor de decimal para algarismo romanos funcionando

--- codict-challenge/hackear.js
+++ codict-challenge/hackear.js
@@ -203,6 +203,37 @@ nextBiggerNumber(999);
     test 2 -> (9) => "IX"
 
 */
+function romanNumerals(n) {
+
+    const RomanNumberMap = {
+        u: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], //Unidade
+        d: ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], //Dezena
+        c: ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], //Centena
+        m: ["M", "MM", "MMM", "MMMM", "MMMMM"] // Milhar
+    };
+
+    if (n > 0 && n < 5000) {
+        let str = Number(n).toString();
+        let arry = str.split('').reverse();
+
+        let uni = Number(arry[0]);
+        let dez = Number(arry[1]);
+        let cen = Number(arry[2]);
+        let mil = Number(arry[3]);
+
+        arry[0] = RomanNumberMap.u[uni - 1] ?? "";
+        arry[1] = RomanNumberMap.d[dez - 1] ?? "";
+        arry[2] = RomanNumberMap.c[cen - 1] ?? "";
+        arry[3] = RomanNumberMap.m[mil - 1] ?? "";
+
+        let response = arry.reverse().join('');
+
+        return console.log(response);
+
+    } else {
+        return console.error("valor inválido")
+    };
+}
 
 /*
     010 - pig latin
--- fundamentos/script.js
+++ fundamentos/script.js
@@ -7,32 +7,37 @@ console.warn("console.warn");
 
 
 
-function RomanNumber(n) {
+function DecToRoman(n) {
 
     const RomanNumberMap = {
-        u:["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], //Unidade
-        d:["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], //Dezena
-        c:["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], //Centena
-        m:["M"] // Milhar
+        u: ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"], //Unidade
+        d: ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"], //Dezena
+        c: ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"], //Centena
+        m: ["M", "MM", "MMM", "MMMM", "MMMMM"] // Milhar
     };
 
-    let str = Number(n).toString();
-    let arry = str.split('').reverse();
+    if (n > 0 && n < 5000) {
+        let str = Number(n).toString();
+        let arry = str.split('').reverse();
 
-    let uni = Number(arry[0]);
-    let dez = Number(arry[1]);
-    let cen = Number(arry[2]);
-    let mil = Number(arry[3]);
-    
-    arry[0] = RomanNumberMap.u[uni - 1] ?? "";
-    arry[1] = RomanNumberMap.d[dez - 1] ?? "";
-    arry[2] = RomanNumberMap.c[cen - 1] ?? "";
-    arry[3] = RomanNumberMap.m[mil - 1] ?? "";
+        let uni = Number(arry[0]);
+        let dez = Number(arry[1]);
+        let cen = Number(arry[2]);
+        let mil = Number(arry[3]);
 
-    let response = arry.reverse().join('');
+        arry[0] = RomanNumberMap.u[uni - 1] ?? "";
+        arry[1] = RomanNumberMap.d[dez - 1] ?? "";
+        arry[2] = RomanNumberMap.c[cen - 1] ?? "";
+        arry[3] = RomanNumberMap.m[mil - 1] ?? "";
 
-    return console.log(response);
+        let response = arry.reverse().join('');
+
+        return console.log(response);
+
+    } else {
+        return console.error("valor inválido")
+    };
 }
 
-RomanNumber(4);
+
 
