92bef3281e781ff2ae22272ff3e87c7191f4dc66
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-06 20:35:26 -0300

    conceito de pilhas checked

---
+++ Estruturas-de-dados/main.js
@@ -0,0 +1,13 @@
+import { Pilha } from "./pilhas";
+
+let names = new Pilha();
+
+names.push('bruno');
+names.push('heleno');
+names.push('carol');
+
+// names.pop();
+// names.isEmpty();
+// names.peek();
+// names.clear();
+// names.listar();
