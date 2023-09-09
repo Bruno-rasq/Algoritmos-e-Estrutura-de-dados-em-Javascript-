2692a397893bbeb8afd425b7e081772ed4c53e2b
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-08 21:50:46 -0300

    estruturas de dados Arrays
    arrays já utilizo bastante...

---
+++ Estruturas-de-dados/arrays/arrays.js
@@ -0,0 +1,14 @@
+let arrayA = [];
+
+arrayA.push("html");
+arrayA.push("css");
+arrayA.push("javascript");
+arrayA.push("sass");
+arrayA.push("node");
+
+for (let i = 0; i < arrayA.length; i++){
+    console.log(arrayA[i])
+};
+
+console.log(arrayA)
+
