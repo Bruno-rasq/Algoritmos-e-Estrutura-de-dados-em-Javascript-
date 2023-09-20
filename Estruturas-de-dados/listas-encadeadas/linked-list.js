db1ecea7cf928dc1438590f8582626b5e98d99f6
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-09-19 21:09:39 -0300

    implementando uma lista encadeada com javascript
    adicionando nó

---
+++ Estruturas-de-dados/listas-encadeadas/linked-list.js
@@ -0,0 +1,50 @@
+// implementando uma lista encadeade.
+
+function linkedList(){
+
+    let head = null; // ponto inicial
+    let length = 0; // tamanho 
+
+    // cada node possue um valor e um pronteiro indicando o proximo
+    const Node = (value) => {
+        return {
+            value,
+            next: null
+        }
+    };
+
+    // funçã para adicionar um novo nó
+    const add = (value) => {
+
+        if(!head){
+            head = Node(value);
+            length++
+            return head
+        }
+
+        let node = head
+        while(node.next){
+            node = node.next
+        }
+        node.next = Node(value)
+        length++
+        return node.next
+    };
+
+    // public
+    return{
+        length: () => length, //retorna o tamanho da lista
+        add: (value) => add(value), //adiciona um novo nó
+        print: () => console.log(head) //imprime o head da lista
+    };
+
+};
+
+const list = linkedList();
+
+list.add(1);
+list.add(2);
+list.add(3);
+
+console.log(list.length());
+list.print()
