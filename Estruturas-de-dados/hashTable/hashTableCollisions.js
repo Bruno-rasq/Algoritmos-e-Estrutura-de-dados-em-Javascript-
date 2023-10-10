4b1f2933875d84ab7a431f6b3d513077ca667762
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-10-09 21:44:41 -0300

    implementando uma tabela hash
    algumas coisas não funcionaram exatamente como deviam...

---
+++ Estruturas-de-dados/hashTable/hashTableCollisions.js
@@ -0,0 +1,83 @@
+class hashtable {
+
+    constructor(size){
+        this.table = new Array(size),
+        this.size = size
+    };
+    
+    hash(key){
+        let total = 0
+        for(let i = 0; i < key.length; i++ ){
+            total += key.charCodeAt(i)
+        }
+        return total % this.size
+    };
+
+
+    set(key, value){
+        let index = this.hash(key)
+        let bucket = this.table[index]
+
+        if(!bucket){
+            this.table[index] = [[key, value]]
+
+        } else {
+            let sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem){
+                // sameKeyItem[1] = value 
+                sameKeyItem.push(value)
+            } else {
+                bucket.push([key, value])
+            }
+        }
+    };
+
+    get(key){
+        const index = this.hash(key)
+        // return this.table[index]
+
+        let bucket = this.table[index]
+        if(bucket){
+            let sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem){
+                return sameKeyItem[1]
+            }
+        };
+
+        return undefined
+    };
+
+    remove(key){
+        const index = this.hash(key)
+        // this.table[index] = undefined
+
+        let bucket = this.table[index]
+        if(bucket){
+            let sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem){
+                bucket.splice(bucket.indexOf(sameKeyItem), 1)
+            } 
+        }
+    };
+
+    display(){
+        for(let i = 0; i < this.table.length; i++){
+            if(this.table[i]){
+                console.log(i, this.table[i])
+            }
+        }
+    };
+
+}
+
+const table = new hashtable(50);
+
+table.set('name', 'Bruno')
+table.set('name', 'johnDoe') 
+table.set('age', 22)
+
+table.display()
+
+// console.log(table.get('name'))
+
+table.set('name', 'Diana') 
