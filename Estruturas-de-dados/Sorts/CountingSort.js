ecd054571c30e4fbc8c4e6e5ad20af78dabf0e43
Rasquinha_ <brunodiasrasquinha@gmail.com>
2023-10-11 19:52:16 -0300

    implementando uma ordenação contada

--- Estruturas-de-dados/Sorts/CountingSort.js
+++ Estruturas-de-dados/Sorts/CountingSort.js
@@ -5,7 +5,6 @@ function CountingSort(arry){
     let count = [];
     let i, z = 0;
     let max = Math.max(...arry);
-    let output = [];
 
     //inicializa o contador..
     for(i = 0; i <= max; i++){
@@ -22,13 +21,50 @@ function CountingSort(arry){
         }
     };
 
-    for(i = 0; i < arry.length; i++){
-        output[i] = arry[i]
-    };
-
-    return output;
+    return arry;
 
 };
 
 let Sort = CountingSort(numbers)
 console.log(Sort)
+
+
+
+
+
+//testes...
+
+let c = [3, 3, 5, 2, 9, 9];
+
+let counting = []
+let maxC = Math.max(...c)
+let i, z = 0
+
+console.log(`Array: ${c.join(' ')}`)
+console.log(`max: ${maxC}`)
+
+//inicia contgem
+for(i = 0; i<=maxC; i++){
+    counting[i] = 0;
+}
+console.log(`count part1 : ${counting.join(' ')}`)
+
+//posição referente ao elemento recebe a quantidade de elementos
+for(i = 0; i<c.length; i++){
+    counting[c[i]]++
+}
+console.log(`count part2 : ${counting.join(' ')}`)
+
+
+//
+for(i = 0; i <= maxC; i++){
+    while(counting[i]-- > 0){
+        c[z++] = i;
+    }
+};
+console.log(
+    `count part3 : ${counting.join(' ')}`
+)
+console.log(
+    `array Ordenado: ${c.join(' ')}`
+)
